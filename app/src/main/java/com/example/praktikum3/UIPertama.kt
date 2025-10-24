853e36ba12369806a6ae9c083074116390b24b09
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-10-24 10:03:27 +0700

    Membuat kompenen text pertamna dalam column dengan memanggil prodi di string

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -35,5 +35,12 @@ fun ActivitasPertama(modifier: Modifier){
             .padding(top = 100.dp)
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    )
+    ){
+        Text(
+            text = stringResource(id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+    }
 }
