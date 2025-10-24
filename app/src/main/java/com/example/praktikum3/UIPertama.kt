3d1c67a07618ee00d052440a3c65ee587bcb293f
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-10-24 10:22:49 +0700

    menggubah pada bagian card menjadi fillmaxwidth

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -48,7 +49,7 @@ fun ActivitasPertama(modifier: Modifier){
         Spacer(modifier = Modifier.height(height = 25.dp))
         Card (
             modifier = Modifier
-                .fillMaxSize(fraction = 1f)
+                .fillMaxWidth(fraction = 1f)
                 .padding(all = 12.dp),
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
